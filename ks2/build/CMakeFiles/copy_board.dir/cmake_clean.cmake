dfcb413140bdfc9c82144d0da9094bc61e7e915b
Ryan Owens <ryanvader82@gmail.com>
2013-05-29 17:54:55 -0500

    Build on May 29

--- ks2/build/CMakeFiles/copy_board.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/copy_board"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/copy_board.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- ks2/build/CMakeFiles/copy_target.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/copy_target"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/copy_target.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
